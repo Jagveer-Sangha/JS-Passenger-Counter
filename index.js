3dfe2b1be963c1454256262941093628fe7140a4
Jagveer Sangha <jagveersangha@gmail.com>
2021-06-11 21:38:32 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// document.getElementById("count").innerText = 5
+// let count = 5
+// console.log(count)
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    // console.log("The button was clicked")
+    count = count + 1
+    countEl.textContent = count
+    // console.log(count)
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    console.log(count)
+    countEl.textContent = 0
+    count = 0
+}
 
